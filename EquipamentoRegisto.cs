5f5b5f83979b3c391ae8a87e6f7d2e5cd9b40f93
Nana <annascwo@gmail.com>
2021-07-17 04:29:08 +0100

    + new class

---
+++ EquipamentoRegisto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biblioteca
+{
+    class EquipamentoRegisto
+    {
+        private string nome;
+        private Equipamento equipamento;
+        private int quantidade;
+        private string exemplares;
+
+        public string Nome { get => nome; set => nome = value; }
+        public Equipamento Equipamento { get => equipamento; set => equipamento = value; }
+        public int Quantidade { get => quantidade; set => quantidade = value; }
+        public string Exemplares { get => exemplares; set => exemplares = value; }
+
+        public EquipamentoRegisto(string nome, Equipamento equipamento, int quantidade, string exemplares)
+        {
+            this.nome = nome;
+            this.equipamento = equipamento;
+            this.quantidade = quantidade;
+            this.exemplares = exemplares;
+        }
+
+        public EquipamentoRegisto(string nome, Equipamento equipamento, int quantidade)
+        {
+            this.nome = nome;
+            this.equipamento = equipamento;
+            this.quantidade = quantidade;
+        }
+    }
+}
---
+++ Nota.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biblioteca
+{
+    class Nota
+    {
+        private string autor;
+        private string titulo;
+        private string subtitulo;
+        private string conteudo;
+
+        public string Autor { get => autor; set => autor = value; }
+        public string Titulo { get => titulo; set => titulo = value; }
+        public string Subtitulo { get => subtitulo; set => subtitulo = value; }
+        public string Conteudo { get => conteudo; set => conteudo = value; }
+
+        public Nota(string autor, string titulo, string subtitulo, string conteudo)
+        {
+            this.autor = autor;
+            this.titulo = titulo;
+            this.subtitulo = subtitulo;
+            this.conteudo = conteudo;
+        }
+
+        public Nota(string autor, string titulo, string conteudo)
+        {
+            this.autor = autor;
+            this.titulo = titulo;
+            this.conteudo = conteudo;
+        }
+    }
+}
